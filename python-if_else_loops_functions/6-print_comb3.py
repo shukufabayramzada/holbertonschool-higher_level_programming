6b4480c7e72f17d549caf811c21219e6aa2b7c1c
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-14 11:21:17 +0000

    print all smallest combinations of two numbers

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for n in range(9):
-    for i in range(1,10):
+    for i in range(1, 10):
         if i != n and n < i:
-            print("{}{}".format(n, i), end =", " if (n * 10 + i) < 89 else "\n")
+            print("{}{}".format(n, i), end=", " if (n * 10 + i) < 89 else "\n")
