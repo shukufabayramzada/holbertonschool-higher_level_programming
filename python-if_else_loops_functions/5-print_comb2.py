26980c62a09c6902a444ce0a3d04e7532602f481
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-14 10:25:03 +0000

    print numbers from 00 to 99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 for n in range(100):
-    if n < 10:
-        print(f"0{n}, ", end = '')
-    elif 10 <= n < 99:
-        print(f"{n}, ", end ='')
-    else:
-        print(f"{n}")
+    print("{:02d}".format(n), end=", " if n < 99 else "\n")
+
