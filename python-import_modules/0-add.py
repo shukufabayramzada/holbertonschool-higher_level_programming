43f8d3b8e38977303dded008db6b38447be40d5f
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-15 14:42:52 +0000

    update function for edge case

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-from add_0 import add
-print("{} + {} = {}".format(a, b, add(a, b)))
-
-
+if __name__ = "__main__":
+    a = 1
+    b = 2
+    from add_0 import add
+    print("{} + {} = {}".format(a, b, add(a, b)))
