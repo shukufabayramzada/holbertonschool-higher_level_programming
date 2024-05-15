f2ed59dc26d97b8dad568f9b31e15c9b48347b57
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-15 19:59:27 +0000

    update

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-    a = 1
-    b = 2
-    from add_0 import add
-    print("{} + {} = {}".format(a, b, add(a, b)))
+a = 1
+b = 2
+from add_0 import add
+print("{} + {} = {}".format(a, b, add(a, b)))
