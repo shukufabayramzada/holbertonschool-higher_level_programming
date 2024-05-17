d150aedf36b11b45532ec021b50fe6922b430034
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-17 18:37:20 +0000

    comprehensive calculator

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
         elif sys.argv[2] == "-":
             print("{} - {} = {}".format(a, b, sub(a, b)))
         elif sys.argv[2] == "*":
-            print("{} + {} = {}".format(a, b, mul(a, b)))
+            print("{} * {} = {}".format(a, b, mul(a, b)))
         elif sys.argv[2] == "/":
-            print("{} + {} = {}".format(a, b, div(a, b)))
+            print("{} / {} = {}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
