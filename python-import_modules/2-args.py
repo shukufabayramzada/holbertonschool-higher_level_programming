26c4178ce7ec29561e4d7413f7c3d964411bc0f9
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-17 14:02:08 +0000

    select arguments and print

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import sys
-n = len(sys.argv)
-s = n - 1
-if s == 0:
-    print("0 arguments.")
+if __name__ == "__main__":
+    n = len(sys.argv)
+    s = n - 1
+    if s == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(s))
 
-for i in range(1, s):
-    print("{} argument:".format(i))
-    print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, n):
+        print("{}: {}".format(i, sys.argv[i]))
