2b26fcf74380b40fcd06aabf2a306dbecb4c9b38
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-17 18:30:35 +0000

    comprehensive calculator

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 
 import sys
-import calculator_1
+import importlib
 
 if __name__ == "__main__":
     n = len(sys.argv)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
+
+    calculator_1 = importlib.import_module('calculator_1')
+
     if n != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
         if sys.argv[2] == "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
+            print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
         elif sys.argv[2] == "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
+            print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
         elif sys.argv[2] == "*":
-            print("{} + {} = {}".format(a, b, mul(a, b)))
+            print("{} + {} = {}".format(a, b, calculator_1.mul(a, b)))
         elif sys.argv[2] == "/":
-            print("{} + {} = {}".format(a, b, div(a, b)))
+            print("{} + {} = {}".format(a, b, calculator_1.div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
