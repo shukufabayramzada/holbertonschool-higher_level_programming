c70cabc6e0044c02c86780ee9dddf1b2ab51ba4c
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-21 12:50:17 +0000

    select max

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    for i in range(len(my_list)):
-        for j in range(1, len(my_list)):
-            if my_list[i] > my_list[j]:
-                return my_list[i]
-            else:
-                return my_list[j]
+    n = len(my_list)
+    if n == 0:
+        return None
+
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if my_list[j] > my_list[j+ 1]:
+                my_list[j], my_list[j+1] = my_list[j + 1], my_list[j]
+
+    return my_list[-1]
