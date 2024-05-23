9c10dc8a26fcdb0eaf01f505fdb427e86717dc60
shukufabyarmzada <shukufabyarmzada@gmail.com>
2024-05-23 18:31:13 +0000

    update for task

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key not in a_dictionary:
-        a_dictionary.setdefault(key, value)
-    else:
-        a_dictionary.update({'key': 'value'})
+    a_dictionary[key] = value
     return a_dictionary
             
