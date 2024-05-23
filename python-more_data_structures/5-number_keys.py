b20536a652a716461878a0dce4239ef1734340f8
shukufabyarmzada <shukufabyarmzada@gmail.com>
2024-05-23 17:22:50 +0000

    function that prints number of keys

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+def number_keys(a_dictionary):
+    count = 0
+    for key in a_dictionary:
+        count += 1
+        if isinstance(a_dictionary[key], dict):
+            count += number_keys(a_dictionary[key])
+    return count
