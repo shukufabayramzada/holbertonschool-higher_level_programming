abe7760fe7968297e7e5056dc5afa243e702c7fe
shukufabyarmzada <shukufabyarmzada@gmail.com>
2024-05-23 18:29:35 +0000

    update or create new key value pair

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if key not in a_dictionary:
+        a_dictionary.setdefault(key, value)
+    else:
+        a_dictionary.update({'key': 'value'})
+    return a_dictionary
+            
