ca08964c4d3861dc3f4504861ee981c5558153a7
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-24 12:46:31 +0000

    update for pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,9 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                        'C': 100, 'D': 500, 'M': 1000}
+    roman_numerals = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000}
     integer_value = 0
     prev_value = 0
 
