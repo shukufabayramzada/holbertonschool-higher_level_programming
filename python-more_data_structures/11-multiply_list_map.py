28ac91c91d946c221602f055334bdc9c683d6576
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-24 11:47:32 +0000

    multiply by number

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_list = my_list.copy()
-    result = list(map(lambda x: x * number, new_list))
-    return result
-
+    new_list = list(map(lambda x: x * number, my_list))
+    return new_list
