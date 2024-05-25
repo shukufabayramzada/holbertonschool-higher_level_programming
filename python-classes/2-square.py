08377ae4b42a891c777071e2f51396b6c19f3f2d
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-25 18:35:22 +0000

    update except message

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
         """Initialize the Square with a given size."""
 
         if not isinstance(size, int):
-            raise TypeError("size must be integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
