51a3cbf145a71ba20b2ae1aca6a60f7bde1796f6
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-25 18:32:30 +0000

    change condition

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,6 +10,6 @@ class Square:
 
         if not isinstance(size, int):
             raise TypeError("size must be integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
