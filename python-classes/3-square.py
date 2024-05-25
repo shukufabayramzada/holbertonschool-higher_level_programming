f8805cf62d88288cfa1f62fece86103150f334f7
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-25 18:38:09 +0000

    calculates the area of the square

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module containing the definition of the Square class."""
+
+
+class Square:
+    """An class that defines a square"""
+
+    def __init__(self, size=0):
+        """Initialize the Square with a given size."""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+def area(self):
+    """Returns the area of the square"""
+    return self.__size**2
