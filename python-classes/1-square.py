8509133af5654446c977c81298ce23d7ab4593af
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-25 21:47:47 +0400

    initilaize with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module containing the definition of the Square class."""
+
+
+class Square:
+    """An empty class that defines a square"""
+    
+    def __init__(self, size):
+        """Initialize the Square with a given size."""
+        self.size = size
