38638fe34680c75909183a04f72974794ab2cbcb
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-26 11:12:32 +0000

    print square based on its position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -32,6 +32,7 @@ class Square:
         """Print square"""
         if self.__size == 0:
             print("")
+            return
         for _ in range(self.__position[1]):
             print("")
         for _ in range(self.__size):
