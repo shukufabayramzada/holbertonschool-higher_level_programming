681363012f3a8a8eeabfe2dfe1c9c6018b6b8db6
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-26 12:31:18 +0000

    square matrix without using for or while

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    result = list(map(lambda row: list(map(lambda i: i ** 2, row)), matrix))
-    return result
+    return list(map(lambda row: list(map(lambda i: i ** 2, row)), matrix))
