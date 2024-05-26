19982c3aa4ead8ab826bc1413b9a89f137ec2c04
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-26 11:48:38 +0000

    magic calculation

---
+++ python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+        except Exception:
+            pass
+        else:
+            result += (a ** b) / i
+    result += a + b
+    return result
