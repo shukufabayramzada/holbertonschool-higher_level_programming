001d4274abacc888a0a2c58961f7c1f6704ea898
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-26 12:08:14 +0000

    function with arguments

---
+++ python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return raesult
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
