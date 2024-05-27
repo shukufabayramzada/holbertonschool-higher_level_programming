502f3b61910e42cae9d593989c91c393bdf092f0
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-27 14:10:52 +0000

    update for all

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""This module defines addition of two float after converting to integers"""
+"""0-add_integer.py and tests/0-add_integer.txt"""
+
 
 def add_integer(a, b=98):
     """
-    Returns an integer: the addition of a and b
-    
-    a and b must be first casted to integers if they are float
-
-    Expected Errors:
-    
-    TypeError: a and b must be integers or floats, otherwise raise
-
-    a TypeError exception with the message a must be an integer or b must be an integer
+    adds an integer
+    unit tests located in tests/0-add_integer.txt
+    checks for type errors
     """
-    if (not isinstance(a, int) and not isinstance(a, float)):
-        raise TypeError("a must be an integer or float")
-    if (not isinstance(b, int) and not isinstance(b, float)):
-        raise TypeError("b must be an integer or float")
-    return (int(a) + int(b))
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
