5f42dac37e0206c8b8a34989cd0176ebda9e9e98
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-27 18:45:20 +0000

    printing first and last name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module represent printing of first and last name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+      Print first and last name of user
+
+      Possible Error:
+       TypeError
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
