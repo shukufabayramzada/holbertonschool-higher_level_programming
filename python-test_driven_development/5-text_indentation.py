b1f8766070b664fe652b48bf9625505ad7f2c5c1
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-28 09:16:48 +0000

    text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/pyhton3
+def text_indentation(text):
+    if not isinstance(text, str):
+         raise TypeError("text must be a string")
+
+    result = ''
+    n = len(text)
+    i = 0
+
+    while i < n:
+        result += text[i]
+        if text[i] in ['.', '?', ':']:
+            result += '\n\n'
+            i += 1
+            while i < n and text[i] == ' ':
+                i += 1
+            continue
+        i += 1
+
+    result_lines = result.split('\n')
+    stripped_lines = [line.strip() for line in result_lines]
+    final_result = '\n'.join(stripped_lines)
+
+    print(final_result)
