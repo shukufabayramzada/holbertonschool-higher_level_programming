ec760273259625f05b819b6431bc5c00a27b96b8
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-29 15:09:00 +0000

    initialize rectangle with height and width

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
     """An class that defines a rectangle
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
