fb9e15c3b31dda13ede6e7f83b7b252ffd3ac1db
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-29 15:05:14 +0000

    initialize rectangle with height and width

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle:
         """Set the value to the width of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -36,6 +36,6 @@ class Rectangle:
         """Set the value to the width of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
