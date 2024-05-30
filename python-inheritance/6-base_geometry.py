e323f492404c84f768cabfbba61572958f5d2225
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-30 17:54:18 +0000

    exception about area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module represent creating an empty class"""
+
+
+class BaseGeometry:
+    """A class initialization with public
+    instance method
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
