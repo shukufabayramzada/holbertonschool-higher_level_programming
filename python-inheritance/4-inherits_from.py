b4272399ef6748ccd0c7c1b178681b7a81381888
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-30 17:41:17 +0000

    the object is an instance of a class that inherited (directly or indirectly) from the specified class

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module represents if an object is an instance of a class that inherited from
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Check for the case of being class
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
