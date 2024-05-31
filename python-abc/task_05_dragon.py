e266b63b2bb0e25a918d1ce67a2c737039082914
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-31 15:26:33 +0000

    update for dragon

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -3,10 +3,10 @@ class SwimMixin:
     def swim(self):
         print("The creature swims!")
 
-    class FlyMixin:
-        def fly(self):
-            print("The creature flies!")
+class FlyMixin:
+    def fly(self):
+        print("The creature flies!")
 
-    class Dragon(SwimMixin, FlyMixin):
-        def roar(self):
-            print("The dragon roars!")
+class Dragon(SwimMixin, FlyMixin):
+    def roar(self):
+        print("The dragon roars!")
