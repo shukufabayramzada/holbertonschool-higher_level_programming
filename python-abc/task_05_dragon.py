d4b46168c3b5a1ebb67de57dd379475de90afb34
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-31 15:20:46 +0000

    dragon class was created

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class SwimMixin:
+    def swim(self):
+        print("The creature swims!")
+
+    class FlyMixin:
+        def fly(self):
+            print("The creature flies!")
+
+    class Dragon(SwimMixin, FlyMixin):
+        def roar(self):
+            print("The dragon roars!")
