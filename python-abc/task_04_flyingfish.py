edcd4fd5d785f7a8714387baf78b6d7ea07b001c
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-31 15:15:39 +0000

    flying fish task

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+class Fish:
+
+    def swim(self):
+        print("The fish is swimming")
+
+    def habitat(self):
+        print("The fish lives in water")
+
+
+class Bird:
+
+    def fly(self):
+        print("The bird is flying")
+
+    def habitat(self):
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    def fly(self):
+        print("The flying fish is soaring")
+
+    def swim(self):
+        print("The flying fish is swimming")
+
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
