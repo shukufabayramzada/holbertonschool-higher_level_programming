b3c01d9d0efaf961206c1e2e3e87793a18e8fdad
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-02 13:52:12 +0000

    adding new attribute dynamically

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+"""
+This module represents  adding new atrribute to the object
+if it`s possible
+"""
+
+
+def add_attribute(obj, key, value):
+    """
+    Adds new attribute if it is possible
+    """
+    if "__dict__" not in dir(obj):
+        raise TypeError("can't add new attribute")
+    obj.__dict__[key] = value
