5a804c789ef0e58a145d80a19ff2687dd96a2ab2
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-03 12:09:30 +0000

    write and return number of characters

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,6 +6,10 @@ This module represents writes a string to a text file
 
 
 def write_file(filename="", text=""):
+    """
+    Write the text to the file
+    and return the number of characters
+    """
     with open(filename, 'w', encoding='utf-8') as f:
         f_contents = f.write(text)
         return f_contents
