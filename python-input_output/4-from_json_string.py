18c47b508e1e79f44c8bb0729a935e0d181e92e6
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-03 14:49:39 +0000

    objectt reprsented by json string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module represents
+returning an object reprsented by a JSON
+string
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Return the object reprsenented
+    by a JSON string
+    """
+    return dict(json.dumps(my_str))
