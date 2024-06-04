7963139e611afd0f367bc0bfdc26064fbb457ed5
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-04 13:55:14 +0000

    update function in the cse for not importing json manually

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -5,8 +5,15 @@ the dictionary description with simple data structure
 for JSON serialization of an object
 """
 
-import json
-
 
 def class_to_json(obj):
-    return json.dumps(obj.__dict__)
+    json_obj = {}
+
+    for key, value in obj.__dict__.items():
+        json_obj[key] = value
+
+    return json_obj
+
+    # Without importing module it can be written as
+    # in the following way by imporrting json
+    # return json.dumps(obj.__dict__)
