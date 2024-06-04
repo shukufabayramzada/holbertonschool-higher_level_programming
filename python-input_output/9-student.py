ca2ba406bd3f067b1fb2c2c222ec88815ba86d10
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-04 14:07:09 +0000

    retrieves a dictionary representation of a Student instance

---
+++ python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module represents retrieving a dictionary representation of a Student instance
+"""
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
