70bc61f95745890445202f1b454f82b7f51144ac
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-04 12:42:03 +0000

    adds all arguments to a Python list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,7 +9,5 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-def add_item(add_item.json):
-    loads = load_from_json_file('add_item.json')
-    save_to_json_file((loads + sys.argv[1:]), add_item.json)
+loads = load_from_json_file('add_item.json')
+save_to_json_file((loads + sys.argv[1:]), add_item.json)
