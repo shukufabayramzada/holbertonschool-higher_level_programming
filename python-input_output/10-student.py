7e6f78a3a51a590414833ba23ae1fcbca74c7062
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-04 14:25:35 +0000

    updated version of retrieving class attribute

---
+++ python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module represents retrieving a
+dictionary representation of a Student instance
+"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs == None:
+            return self.__dict__
+
+        if not isinstance(attrs, list):
+            return self.__dict__
+
+        filtered_dict = {}
+
+        for attr in attrs:
+            if isinstance(attr, str) and attr in self.__dict__:
+                filtered_dict[attr] = self.__dict__[attr]
+
+        return filtered_dict
