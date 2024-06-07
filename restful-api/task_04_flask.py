f515ae68182b32e9a8c8bca97d2651c4d9cb459e
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-08 00:22:33 +0400

    trying.......

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,33 +1,30 @@
-from flask import Flask, jsonify, request
+#!/usr/bin/python3
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# In-memory dictionary to store users
-users = {
-    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
-    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
-}
+all_users = {}
 
 @app.route('/')
 def home():
-    return "Welcome to the Flask API!"
+    return 'Welcome to the Flask API!'
 
 @app.route('/data')
-def get_data():
-    usernames = list(users.keys())
+def data():
+    usernames = list(all_users.keys())
     return jsonify(usernames)
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
     if request.method == 'POST':
         user = request.get_json()
-        users[user['username']] = user
+        all_users[user['username']] = user
         return jsonify({"message": "User added", "user" : user})
 
 @app.route('/users/<username>')
 def user(username):
-    if username in users:
-        return jsonify(users[username])
+    if username in all_users:
+        return jsonify(all_users[username])
     else:
         return jsonify({"error": "User not found"}), 404
 
