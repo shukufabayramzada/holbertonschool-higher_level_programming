11c31a295a3e7202dcf772f6ef858e274a28e2fd
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-29 00:15:38 +0400

    count number of shows for each genre

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM genres
+JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.name
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
