0a8b0a77fa834cc6925334ca1ebaf3564545e482
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-28 16:59:29 +0400

    create user with all privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Script that creates the MySQL server user user_0d_1 with all priveleges on MySQL server.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_123';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES
+FLUSH PRIVILEGES;
