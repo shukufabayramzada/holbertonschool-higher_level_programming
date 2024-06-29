09d5a3a0827bb03173ce0454339c0fbc954f184e
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-29 19:13:33 +0400

    fixed syntax error

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -19,8 +19,8 @@ if __name__ == "__main__":
             db=database_name
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY id ASC""")
     result = cur.fetchall()
 
     for row in result:
