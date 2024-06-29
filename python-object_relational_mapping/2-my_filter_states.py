5b2f8088b64c8c88602a4582f58fcf746c258d63
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-29 19:24:57 +0400

    selection with formatted string

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Sript that takes in an argument and displays all values in the state table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    searched_state = sys.argv[4]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name
+    )
+    cur = db.cursor()
+    query = """SELECT * FROM states WHERE name = '{}'
+            ORDER BY id ASC""".format(searched_state)
+    cur.execute(query)
+    result = cur.fetchall()
+
+    for row in result:
+        print(row)
+
+    cur.close()
+    db.close()
