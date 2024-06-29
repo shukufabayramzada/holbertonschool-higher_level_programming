30422eb4b26e46a8aa96bc2a4095a4683a4767cd
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-29 18:36:09 +0400

    update

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    db = MySQL.connect(
+    db = MySQLdb.connect(
             host="localhost",
             port=3306,
             user=mysql_username,
