8f29ea554658448dd4c0ca3fc14d127072d59888
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-29 18:30:04 +0400

    select states with id

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQL.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    result = cur.fetchall()
+
+    for row in result:
+        print(row)
+
+    cur.close()
+    db.close()
