6988f60e91bb061bd29acd8cc18b7e2c2b949273
shukufabayramzada <shukufabayramzada@gmail.com>
2024-06-29 19:05:46 +0400

    select states starting with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    result = cur.fetchall()
+
+    for row in result:
+        print(row)
+
+    cur.close()
+    db.close()
