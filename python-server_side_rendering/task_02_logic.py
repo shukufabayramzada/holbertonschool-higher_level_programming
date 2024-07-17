82f3733b39f8e8a8ab744b67ed5b997025a701cb
shukufabayramzada <shukufabayramzada@gmail.com>
2024-07-17 13:03:45 +0400

    trying to resolve the error

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
 import json
+import os
+
 app = Flask(__name__)
 
 app.route('/items')
-def item():
-    with open('items.json') as f:
-        data = json.load(f)
+def items():
+    try:
+        with open('items.json') as f:
+            data = json.load(f)
+            items = data.get('items', [])
+            return render_template('items.html', items = items)
+    except FileNotFoundError:
+        return "Items file not found", 404
+    except json.JSONDecodeError:
+        return "Error decoding JSON", 500
         
-    items = data.get('items', [])
-    return render_template('items.html', items = items)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
